481b1fc55c2144c60cc000f3ae48aa466fab756f
Vernon <vernonr@gmail.com>
2023-06-26 14:16:54 +0100

    Documentation added to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import csv
 
+'''
+This script uses the frodo number to discover which frodos are live on the network
+'''
 
 def writeToCSV(filename, row):
     with open(filename, "a", newline='') as f:
@@ -24,7 +27,6 @@ for combo in prouter_combos:
                 interface_string = str(interface)
             frodo_number = combo + '0' + str(linecard) + interface_string
             frodo_fqdn = prefix + frodo_number + dns_suffix
-            # print(frodo_fqdn)
             r = os.system("ping -n 1 -w 10 " + frodo_fqdn)
             row = [frodo_fqdn]
             if r == 0:
